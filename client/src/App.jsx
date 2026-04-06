3d26b3d5e7e3b7f6caea9c0e31e836dfaa4fb2e5
THAMILSELVAN2006 <panjutamil2019@gmail.com>
2026-04-06 11:01:18 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-center text-blue-500">Task Flow</h1>
+    </div>
+  )
+}
+
+export default App
